08582d756be1ba207b54b84433e03123d2eaae19
chiva <chiva@DESKTOP-PU1P2K9>
2020-02-23 14:38:25 -0600

    Cambio en el metodo filterRepeatedTuples de tipo Table a List

--- src/interfaces/ISelection.java
+++ src/interfaces/ISelection.java
@@ -1,8 +1,9 @@
 package interfaces;
 
+import java.util.List;
 import model.Table;
 
 public interface ISelection {
     public Table between(Double lowestValue, Double highestValue, Table table);
-    public Table filterRepeatedTuples(Table table);
+    public List<List<String>> filterRepeatedTuples(List<List<String>> table);
 }
